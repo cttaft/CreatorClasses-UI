1488db9c8f5672441d5581901691a6dcb5e913f4
Charlie Taft <taft.charlie@gmail.com>
2022-05-03 09:44:38 -0500

    fix a tag for build

--- creator-classes-app/components/CreatorDetail.tsx
+++ creator-classes-app/components/CreatorDetail.tsx
@@ -16,7 +16,7 @@ const CreatorDetail: FunctionComponent<Props> = ({ creator, classes }) => {
         <Row>
             <h2>{creator.name}</h2>
 
-            <Row xs={2} md={4} lg={6}><Col><a href={creator.youtubeUrl} target="_blank"><img style={{cursor:'pointer'}} src={creator.imageSrc}></img></a></Col><Col>{creator.description}</Col></Row>
+            <Row xs={2} md={4} lg={6}><Col><a href={creator.youtubeUrl} target="_blank" rel="noreferrer"><img style={{cursor:'pointer'}} src={creator.imageSrc}></img></a></Col><Col>{creator.description}</Col></Row>
             
         </Row>
         <ClassList classes={classes} showUnsubscribe={false}></ClassList>
